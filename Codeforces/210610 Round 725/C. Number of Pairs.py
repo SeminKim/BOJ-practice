3cd9e08ceae135e93cfb624fbb847b4700850c33
SeminKim <68637958+seminkim@users.noreply.github.com>
2021-06-11 17:10:50 +0900

    up-solve codeforces 725 C,D

--- Codeforces/210610 Round 725/C. Number of Pairs.py
+++ Codeforces/210610 Round 725/C. Number of Pairs.py
@@ -1,51 +1,25 @@
 from sys import stdin
-
-def binary_search(seq, start, end, target, findle=True):  #search idx for target or larger
-
-    mid = (start+end) // 2
-    if target <= seq[mid]:
-        return binary_search(seq, start, mid, target)
-    if target > seq[mid]:
-        return binary_search(seq, mid, end, target)
-
+from bisect import *
 
 
 def solve():
-    n, l, r = map(int,input().split())
+    n, l, r = map(int, input().split())
     seq = list(map(int, stdin.readline().split()))
     seq.sort()
 
-
-
     ans = 0
-    for i in range(n-1):  # 0 to n-2 is sufficient
-        left = l - seq[i]  # have to find idx such that seq[idx] <= left
+    for i in range(n - 1):
+        left = l - seq[i]
         right = r - seq[i]
-        p1 = (i + n) // 2
-        stepsize = (n - i) //2
-        while stepsize > 1:
-            if left == seq[p1]:
-                break
-            if left < seq[p1]:
-                p1 -= stepsize
-                stepsize = stepsize //2
-            else:
-                p1 += stepsize
-                stepsize = stepsize //2
+        a = bisect_left(seq, left, lo=i + 1)
+        b = bisect_right(seq, right, lo=i + 1)
 
-        if left
+        ans += b - a
 
-
-
-
-        foo_r = binary_search(seq, i+1, n-1, right)-1
-        foo_l = binary_search(seq, i+1, n-1, left)
-        if foo_r != n and foo_l != -1:
-            ans += foo_r - foo_l
     print(ans)
+
 
 t = int(stdin.readline().strip())
 
 for _ in range(t):
     solve()
-
