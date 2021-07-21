a4836cb5a031808e780eed8914e94ef4d5ce4575
SeminKim <68637958+seminkim@users.noreply.github.com>
2021-07-21 23:28:06 +0900

    up-solve codeforces 724-C

--- Codeforces/210606 Round 724/C. Diluc and Kaeya.py
+++ Codeforces/210606 Round 724/C. Diluc and Kaeya.py
@@ -1,11 +1,12 @@
 from sys import stdin
 from math import gcd
+from collections import defaultdict
 
 
-# Not Solved Yet !!
+# think about grid point on line.
 
-def simplify(x: tuple):
-    a, b = x[0], x[1]
+
+def simplify(a, b):
     if a == 0:
         return 0, 1
     if b == 0:
@@ -16,31 +17,20 @@ def simplify(x: tuple):
 
 def solve():
     n = int(stdin.readline().strip())
-
     seq = list(stdin.readline().strip())
-    partial_sum = [0 for _ in range(n)]
-    ans = [1 for _ in range(n)]
-    cnt = 0
+    save = defaultdict(int)
+
+    num_d = 0
+    num_k = 0
     for i in range(n):
         if seq[i] == 'D':
-            seq[i] = 1
-            cnt += 1
+            num_d += 1
         else:
-            seq[i] = 0
-        partial_sum[i] = cnt
-
-    # ratio = simplify((partial_sum[n-1], n - partial_sum[n-1]))
-    # unit = ratio[0] + ratio[1]
-
-    for i in range(1, n):  # if we make first cut 0 to i
-        ratio = simplify((partial_sum[i], i + 1 - partial_sum[i]))
-        unit = ratio[0] + ratio[1]
-        k = 1
-        while 0 <= i - k * unit:
-            if partial_sum[i] - partial_sum[i - unit] == ratio[0]:
-                ans[i] = ans[i - unit] + 1
-            k += 1
-    print(*ans)
+            num_k += 1
+        ratio = simplify(num_d, num_k)
+        save[ratio] += 1
+        print(save[ratio], end=' ')
+    print()
 
 
 t = int(stdin.readline().strip())
