ca9b06111129d7117d6df3b205a15b15b745f6c2
SeminKim <68637958+seminkim@users.noreply.github.com>
2021-08-07 19:16:14 +0900

    solve 1019

--- BOJ/1019.py
+++ BOJ/1019.py
@@ -1,4 +1,10 @@
 # https://www.acmicpc.net/problem/1019
+# 274 = 200 + 70 + 4
+# 200: [0] += 100, [1] += 100, [2] += 74
+# 70: [0], [1], ... [6] += 10, [7] += 4
+# 4: [0], [1], ... [4] += 1
+# number starts with [0]: 111 : 0xx => 100, 0'0'x => 10, 00'0' => 1
+
 
 from sys import stdin
 
@@ -9,63 +15,28 @@ n_str = str(n)
 def helper(num_digit):  # return number of j > 1 for 00...00~99...99
     if num_digit <= 1:
         return num_digit
-    return helper(num_digit-1) * 10 + 10**(num_digit-1)
-
-
-def helper_zero(num_digit):
-    return helper(num_digit) - int('1'*num_digit)
+    return helper(num_digit - 1) * 10 + 10 ** (num_digit - 1)
 
 
 ans = [0 for _ in range(10)]
 
-
 for idx in range(len(n_str)):
     curr = int(n_str[idx])
+
+    if idx == len(n_str) - 1:
+        for num in range(curr + 1):
+            ans[num] += 1
+        break
+
     lvl = len(n_str) - 1 - idx
     foo = curr * helper(lvl)
     for num in range(10):
         ans[num] += foo
         if num < curr:
-            ans[num] += 10**lvl
+            ans[num] += 10 ** lvl
 
     if lvl != 0:
-        ans[curr] += int(n_str[idx+1:])
+        ans[curr] += int(n_str[idx + 1:]) + 1
 
-print(ans)
-
-
-
-
-
-
-# curr = int(str(n)[:3])
-# ans = base[curr][:]
-# if n < 1000:
-#     print(*ans)
-#     exit()
-#
-#
-# for digit in str(n)[3:]:
-#     digit = int(digit)
-#     for i in range(10):
-#         ans[i] *= 10
-#         ans[i] += curr
-#         if i <= digit:
-#             ans[i] += 1
-#     curr = int(str(curr) + str(digit))
-#
-# print(*ans)
-#
-#
-#
-#
-#
-# for num in range(1, 100001):
-#     for one in str(num):
-#         ans[int(one)] += 1
-#     if num in [2748,274,27,2]:
-#         print(num, ans)
-#
-# ans = 0
-#
-# print(ans)
+ans[0] -= int('1' * (len(n_str)))
+print(*ans)
