2ed5adf74da2fe5b32037b11df5244f1416b35ca
SeminKim <68637958+seminkim@users.noreply.github.com>
2021-09-09 16:53:29 +0900

    upsolve Codeforces Edu Round 113. D

--- Codeforces/210908 Edu Round 113/D.py
+++ Codeforces/210908 Edu Round 113/D.py
@@ -1,13 +1,11 @@
 from bisect import bisect_left
 from sys import stdin
-from collections import deque
 
 
-### NOT SOLVED YET ###
 def solve():
     n, m, k = map(int, stdin.readline().split())
-    verti_line = deque(map(int, stdin.readline().split()))
-    hori_line = deque(map(int, stdin.readline().split()))
+    verti_line = list(map(int, stdin.readline().split()))
+    hori_line = list(map(int, stdin.readline().split()))
     verti = []  # points in vertical line only
     hori = []
     for _ in range(k):
@@ -18,46 +16,53 @@ def solve():
             continue
 
         elif verti_line[idx1] == x:
-            verti.append((y, x))
+            verti.append((idx2, x))
         else:
-            hori.append((x, y))
+            hori.append((idx1, y))
 
-    verti = deque(sorted(verti))
-    hori = deque(sorted(hori))
+    verti = list(sorted(verti))
+    hori = list(sorted(hori))
 
     ans = 0
     # check verti+verti case
-    left = None
-    right = hori_line.popleft()
-    while hori_line:
-        left = right
-        right = hori_line.popleft()
-        temp = 0
-        now_y = -1
-        while len(verti) > 0 and verti[0][0] < right:
-            _, bar = verti.popleft()
-            if now_y != bar:
-                if temp > 1:
-                    ans += temp * (temp - 1) // 2
-                temp = 0
-                now_y = bar
-            temp += 1
+    i, j = 1, 0
+    while i < m + 1 and j < len(verti):
+        count = 0
+        temp = []
+        curr_bar = -1
+        while j < len(verti) and verti[j][0] == i:
+            bar = verti[j][1]
+            count += 1
+            j += 1
+            if bar == curr_bar:
+                temp[-1] += 1
+            else:
+                curr_bar = bar
+                temp.append(1)
+        ans += count * (count - 1) // 2
+        for x in temp:
+            ans -= x * (x - 1) // 2
+        i += 1
 
-    left = None
-    right = verti_line.popleft()
-    while verti_line:
-        left = right
-        right = verti_line.popleft()
-        temp = 0
-        now_x = -1
-        while len(hori) > 0 and hori[0][0] < right:
-            foo, _ = hori.popleft()
-            if now_x != foo:
-                if temp > 1:
-                    ans += temp * (temp - 1) // 2
-                temp = 0
-                now_x = foo
-            temp += 1
+    i, j = 1, 0
+    while i < n + 1 and j < len(hori):
+        count = 0
+        temp = []
+        curr_bar = -1
+        while j < len(hori) and hori[j][0] == i:
+            bar = hori[j][1]
+            count += 1
+            j += 1
+            if bar == curr_bar:
+                temp[-1] += 1
+            else:
+                curr_bar = bar
+                temp.append(1)
+
+        ans += count * (count - 1) // 2
+        for x in temp:
+            ans -= x * (x - 1) // 2
+        i += 1
     print(ans)
 
 
