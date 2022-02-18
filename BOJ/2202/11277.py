f0b5c931e260bf3bd25d7ba60d3ed090aa592171
SeminKim <68637958+seminkim@users.noreply.github.com>
2022-02-18 15:47:07 +0900

    add 11277

---
+++ BOJ/2202/11277.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/11277
+# Bitmasking
+
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+seq = [list(map(int, stdin.readline().split())) for _ in range(m)]
+
+for bit in range(1 << n):
+    for a, b in seq:
+        if a > 0:
+            fst = bit & (1 << (a - 1))
+        else:
+            fst = not (bit & (1 << (-a - 1)))
+        if b > 0:
+            snd = bit & (1 << (b - 1))
+        else:
+            snd = not (bit & (1 << (-b - 1)))
+        if not (fst or snd):
+            break
+    else:
+        print(1)
+        break
+else:
+    print(0)
