ff0b220d505d69186a79e3776632a8429b526657
SeminKim <68637958+seminkim@users.noreply.github.com>
2022-02-23 15:06:31 +0900

    modify 9252
    previous code got MLE after re-judgement

--- BOJ/2106/9252.py
+++ BOJ/2106/9252.py
@@ -9,29 +9,52 @@ second = stdin.readline().strip()
 n = len(first)
 m = len(second)
 
-table = [[0 for _ in range(m+1)] for _ in range(n+1)]
-ansidx = [[[] for _ in range(m+1)] for _ in range(n+1)]
+table = [[0 for _ in range(m)] for _ in range(n)]
 
-for col in range(1,m+1):
-    if first[0] in second[:col]:
-        table[1][col] = 1
-        ansidx[1][col] = [first[0]]
+# Initial lines
+for col in range(m):
+    if first[0] == second[col]:
+        for ncol in range(col, m):
+            table[0][ncol] = 1
+        break
+for row in range(n):
+    if second[0] == first[row]:
+        for nrow in range(row, n):
+            table[nrow][0] = 1
+        break
 
-for row in range(1,n+1):
-    if second[0] in first[:row]:
-        table[row][1] = 1
-        ansidx[row][1] = [second[0]]
+# fill table
+for row in range(1, n):
+    for col in range(1, m):
+        table[row][col] = max(table[row][col - 1], table[row - 1][col],
+                              table[row - 1][col - 1] + (first[row] == second[col]))
+print(table[n - 1][m - 1])  # LCS length
 
-for row in range(2,n+1):
-    for col in range(2,m+1):
-        table[row][col] = table[row][col - 1]
-        ansidx[row][col] = ansidx[row][col - 1]
+# find backward
+row, col = n - 1, m - 1
+curr = table[row][col]
+ans = []
+while (row, col) != (0, 0) and curr > 0:
+    if row > 0 and table[row - 1][col] == curr:
+        row -= 1
+    elif col > 0 and table[row][col - 1] == curr:
+        move = True
+        col -= 1
+    else:
+        ans.append(first[row])
+        row = max(0, row - 1)
+        col = max(0, col - 1)
+        curr = table[row][col]
 
-        if second[col-1] in first[:row]:
-            idx = first[:row].rfind(second[col-1])
-            if table[row][col] < table[idx][col-1] + 1:
-                table[row][col] = table[idx][col-1] + 1
-                ansidx[row][col] = ansidx[idx][col-1] + [second[col-1]]
+if curr == 1:
+    ans.append(first[0])
 
-print(table[n][m])
-print(''.join(ansidx[n][m]))
+print(''.join(reversed(ans)))
+
+# # debug
+# print(" ", end='')
+# for L in second:
+#     print(f"  {L}", end='')
+# print()
+# for L, foo in zip(first, table):
+#     print(L, foo)
